16f77c266cbda579a06bb399c55a54de0d199549
jezreelfranco <jezreel.e.franco@gmail.com>
2024-01-13 20:59:29 -0700

    C# Prep 2-  Added Letter Grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage");
+        string input = Console.ReadLine();
+        int percent = int.Parse(input);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed! Congrats!!");
+        }
+
+        else
+        
+        {
+            Console.WriteLine("You can do it! Better luck next time!");
+        }
     }
 }
