ed19205a5d7f2f4433938b1b4bf3cebd466aeb10
jezreelfranco <jezreel.e.franco@gmail.com>
2024-01-20 23:38:19 -0700

    added prep3 requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Globalization;
 
-class Program
+Console.WriteLine("Hello Prep3 World!");
+
+// ask the user for the magic number
+Console.WriteLine("What is the magic number? ");
+string input = Console.ReadLine();
+int magicNumber = int.Parse(input);
+
+// PART 1 & 2 ask user for their guess
+// Console.WriteLine("What is your guess? ");
+// string input2 = Console.ReadLine();
+// int userGuess = int.Parse(input2);
+
+// PART 3: Random Number Generator
+Random randomGenerator = new Random();
+int magicNumberGenerated = randomGenerator.Next(1, 101);
+
+int guess = -1;
+
+// PART TWO: looping through
+// HIGHER than actual number
+ while (guess != magicNumberGenerated)
 {
-    static void Main(string[] args)
+    // ask user for their guess
+    Console.WriteLine("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+        
+    if (guess > magicNumberGenerated)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Lower");
+
     }
-}
+ //LOWER      
+    else if (guess < magicNumberGenerated)
+    {
+        Console.WriteLine("Higher");
+    }
+    else
+    {
+        Console.WriteLine("You guessed it right!");
+    }
+
+     }
+
+    // PART ONE: HIGHER than actual number:
+        //if (userGuess > magicNumber)
+//         {
+//             Console.WriteLine("Lower");
+
+//             Console.WriteLine("What is your guess?");
+//             string guess2 = Console.ReadLine();
+//             int.Parse(guess2);
+//         }
+//     // PART ONE: LOWER than magic number
+//         else if (userGuess < magicNumber)
+//         {
+//             Console.WriteLine("Higher");
+//         }
+//     // PART ONE: guess right!
+//         else
+//         {
+//             Console.WriteLine("You guessed it right! ");
+//         }
+//     }
+// }
