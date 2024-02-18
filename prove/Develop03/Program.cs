605110142ced0cfd3e0be6604240b90373cc0e79
jezreelfranco <jezreel.e.franco@gmail.com>
2024-02-17 23:56:08 -0700

    requirements

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Net.Quic;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
-{
+{ 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture();
+        scripture.Reference_and_word();
+
+        Console.WriteLine("\n Press Enter to continue or type 'quit' to finish: ");
+        string user_input =Console.ReadLine();
+
+            while (user_input != "quit")
+            {
+                // Console.Clear();
+                Console.WriteLine("\n Press Enter to continue or type 'quit' to finish: ");
+                string input =Console.ReadLine();
+
+                scripture.HideWords();
+
+                if (input == "quit")
+                {
+                    break;
+                }
+            }
     }
+        
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        List<string> _words1 = new List<string> { "This", "is", "a", "sentence" };
+        List<string> _blanks = new List<string> { "_____", "__", "_", "________" };
+
+        Random random = new Random();
+
+        for (int i = 0; i < _blanks.Count; i++)
+        {
+        int randomIndex = random.Next(0, _blanks.Count);
+        string randomBlank = _blanks[randomIndex];
+
+        int wordIndex = randomIndex; 
+
+        _words1.RemoveAt(randomIndex);
+        _words1.Insert(wordIndex, randomBlank);
+        }
+
+        string combined_text_and_blanks = string.Join(" ", _words1);
+        Console.WriteLine(combined_text_and_blanks);
+        
     }
 }
+
+
