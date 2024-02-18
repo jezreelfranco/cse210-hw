5102480f10a0b545ad851330c0aeb25aaac3434d
jezreelfranco <jezreel.e.franco@gmail.com>
2024-02-17 23:55:45 -0700

    requirements

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,36 @@
+using System.Data;
+
+public class Reference
+{
+    private string _book;
+    private string _chapter;
+    private string _verse;
+    private string _endVerse;
+
+    public Reference(string book, string chapter, string verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public Reference(string book, string chapter, string verse, string endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+
+    public string GetSingleReference()
+    {
+        string text = $"{_book} {_chapter}: {_verse}";
+        return text;
+    }
+
+    public string GetMultipleReference()
+    {
+        string text = $"{_book} {_chapter}: {_verse}-{_endVerse}";
+        return text;
+    }
+}
