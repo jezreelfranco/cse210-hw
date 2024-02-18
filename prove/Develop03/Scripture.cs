fb59af0d8ccdf56d043d2ebf22e4618e2d617727
jezreelfranco <jezreel.e.franco@gmail.com>
2024-02-17 23:55:28 -0700

    requirements

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,30 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class Scripture
+{
+    // public Scripture(string reference, string word);
+    Reference reference1 = new Reference("John", "3", "16");
+    Word word1 = new Word();
+        
+    public void Reference_and_word()
+    {
+        Console.WriteLine(reference1.GetSingleReference());
+        word1.GetWordsShown();
+    
+        // Reference reference2 = new Reference("Proverbs", "3", "5", "6");
+        // Console.WriteLine(reference2.GetMultipleReference());
+    }
+
+    public void Is_Completely_Hidden()
+    {
+        Console.WriteLine(reference1.GetSingleReference());
+        // word1.GetIsHidden();
+    }
+
+    public void HideWords()
+    {
+        Console.WriteLine(reference1.GetSingleReference());
+        word1.GetHiddenWord();
+        
+    }
+}
