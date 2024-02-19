da9310031a2f27ccce0fa9b05e019edf69c18f3c
jezreelfranco <jezreel.e.franco@gmail.com>
2024-02-18 22:09:57 -0700

    requirements

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -5,14 +5,14 @@ public class Scripture
     // public Scripture(string reference, string word);
     Reference reference1 = new Reference("John", "3", "16");
     Word word1 = new Word();
+
+    // Reference reference2 = new Reference("Proverbs", "3", "5", "6");
+        // Console.WriteLine(reference2.GetMultipleReference());
         
     public void Reference_and_word()
     {
         Console.WriteLine(reference1.GetSingleReference());
-        word1.GetWordsShown();
-    
-        // Reference reference2 = new Reference("Proverbs", "3", "5", "6");
-        // Console.WriteLine(reference2.GetMultipleReference());
+        word1.GetWordsShown1();
     }
 
     public void Is_Completely_Hidden()
@@ -24,7 +24,6 @@ public class Scripture
     public void HideWords()
     {
         Console.WriteLine(reference1.GetSingleReference());
-        word1.GetHiddenWord();
-        
+        word1.GetHiddenWord1();        
     }
 }
