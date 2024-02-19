cb68134c798dbbb9e5625d2ca175790309f9b560
jezreelfranco <jezreel.e.franco@gmail.com>
2024-02-18 22:15:23 -0700

    Requirements

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -9,6 +9,7 @@ class Program
         Scripture scripture = new Scripture();
         scripture.Reference_and_word();
         scripture.HideWords();
+
         
         // Scripture scripture2 = new Scripture();
         // scripture2.Reference_and_word2();
