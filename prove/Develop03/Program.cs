30e7577f79e7c1dbad097a1db028d3749eec0020
jezreelfranco <jezreel.e.franco@gmail.com>
2024-02-18 22:08:56 -0700

    requirements

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Net.Quic;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
-{
+{ 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture();
+        scripture.Reference_and_word();
+        scripture.HideWords();
+
+        // Scripture scripture2 = new Scripture();
+        // scripture2.Reference_and_word2();
+    
     }
 }
+        
