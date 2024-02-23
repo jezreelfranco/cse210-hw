2740cc645c480610d3a9ef17342260b7857e8e19
jezreelfranco <jezreel.e.franco@gmail.com>
2024-02-23 08:47:54 -0700

    completed requirements

--- prepare/Learning04/WritingAssigment.cs
+++ prepare/Learning04/WritingAssigment.cs
@@ -2,7 +2,7 @@ public class WritingAssignment: Assignment
 {
     private string _title = " ";
 
-    public string GetWritingInfo()
+    public string GetWritingInfo() 
     {
         return $"{_title} by {_name}";
     }
