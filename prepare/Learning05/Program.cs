5eb41fa60f3dc44376cfb03107b3174432b28aa5
jezreelfranco <jezreel.e.franco@gmail.com>
2024-03-05 09:54:17 -0700

    completed prep requirements

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 2);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("red", 2, 3);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle ("purple", 2);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {   
+            string color = shape.GetColor();
+            Console.WriteLine($"The color is {color}");
+
+            double area = shape.GetArea();
+            Console.WriteLine($"The shape's area is: {area}");
+        }
     }
 }
